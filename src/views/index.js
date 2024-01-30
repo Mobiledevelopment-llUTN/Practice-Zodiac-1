12f2af9693bc1639ab8140f800da29989dd3b02e
LilMat69 <semataoe@est.utn.ac.cr>
2024-01-29 19:41:09 -0600

    Second Commit

--- src/views/index.js
+++ src/views/index.js
@@ -1,69 +1,112 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { Image, ScrollView,  Text, View} from 'react-native';
-import {style_01} from '../styles/style_01';
+import {style_01} from '../styles/style_Sign01.js';
 
 const data = [
-    {codigo : 'ITI-121',
-        nombre : 'Fundamentos de Programación',
-        nivel  : 1,
-        creditos : 6},
-    {codigo : 'ITI-122',
-        nombre : 'Comunicación Oral y Escrita',
-        nivel  : 1,
-        creditos : 2},
-    {codigo : 'ITI-123',
-        nombre : 'Fundamentos de Bases de Datos',
-        nivel  : 1,
-        creditos : 3},
-    {codigo : 'ITI-124',
-        nombre : 'Matemáticas para Computación I',
-        nivel  : 1,
-        creditos : 6},
-    {codigo : 'ID-TEC-1',
-        nombre : 'Inglés 1',
-        nivel  : 1,
-        creditos : 0},
-    {codigo : 'IIT-221',
-        nombre : 'Programación I',
-        nivel  : 2,
-        creditos : 6},
-    {codigo : 'IIT-222',
-        nombre : 'Plataformas Tecnológicas I',
-        nivel  : 2,
-        creditos : 3},
-    {codigo : 'IIT-223',
-        nombre : 'Administración de Bases de Datos',
-        nivel  : 2,
-        creditos : 3},
-    {codigo : 'IIT-224',
-        nombre : 'Matemáticas para Computación II',
-        nivel  : 2,
-        creditos : 3},
-    {codigo : 'ID-TEC-2',
-        nombre : 'Inglés 1',
-        nivel  : 2,
-        creditos : 0},
+    {
+        Signo : 'Aries',
+        Dias  : '21 de marzo al 19 de abril. ',
+        Elemento : 'Fuego',
+        Astro: 'Sol',
+        Piedra_Preciosa: 'Ruby',
+    },
+    {
+        Signo : 'Tauro',
+        Dias  : '20 de abril y el 20 de mayo. ',
+        Elemento : 'Tierra',
+        Astro: 'Venus',
+        Piedra_Preciosa: 'Cuarzo Rosa',
+    },
+    {
+        Signo : 'Geminis',
+        Dias  : '21 de mayo y el 20 de junio. ',
+        Elemento : 'Aire',
+        Astro: 'Mercurio',
+        Piedra_Preciosa: 'Ambar',
+    },
+    {
+        Signo : 'Cancer',
+        Dias  : '21 de junio y el 22 de julio ',
+        Elemento : 'Agua',
+        Astro: 'Luna',
+        Piedra_Preciosa: 'Esmeralda',
+    },
+    {
+        Signo : 'Leo',
+        Dias  : '22 de julio y 22 de agosto ',
+        Elemento : 'Fuego',
+        Astro: 'Sol',
+        Piedra_Preciosa: 'Jaspe Rojo',
+    },
+    {
+        Signo : 'Virgo',
+        Dias  : '23 de agosto y el 22 de septiembre ',
+        Elemento : 'Tierra',
+        Astro: 'Mercurio',
+        Piedra_Preciosa: 'Lapislázuli',
+    },
+    {
+        Signo : 'Libra',
+        Dias  : '23 de septiembre y el 22 de octubre. ',
+        Elemento : 'Aire',
+        Astro: 'Venus',
+        Piedra_Preciosa: 'Zafiro',
+    },
+    {
+        Signo : 'Escorpio',
+        Dias  : '23 de octubre al 21 de noviembre ',
+        Elemento : 'Agua',
+        Astro: 'Pluton',
+        Piedra_Preciosa: 'Amatista',
+    },
+    {
+        Signo : 'Sagitario',
+        Dias  : '22 de noviembre y el 21 de noviembre. ',
+        Elemento : 'Fuego',
+        Astro: 'Jupiter',
+        Piedra_Preciosa: 'Citrino',
+    },
+    {
+        Signo : 'Capricornio',
+        Dias  : '21 de marzo al 19 de abril. ',
+        Elemento : 'Tierra',
+        Astro: 'Saturno',
+        Piedra_Preciosa: 'Granate',
+    },
+    {
+        Signo : 'Acuario',
+        Dias  : '21 de diciembre y el 19 de enero ',
+        Elemento : 'Aire',
+        Astro: 'Urano',
+        Piedra_Preciosa: 'Fluorita Verde',
+    },
+    {
+        Signo : 'Piscis',
+        Dias  : '19 de febrero y el 20 de marzo ',
+        Elemento : 'Agua',
+        Astro: 'Neptuno',
+        Piedra_Preciosa: 'Cuarzo Transparente',
+    },
 ];
 
 const Index = () => {
     return (
         <View>
-            <View style={style_01.divHeader}>
-                <Image source={require('../imgs/logos/logo_universidad.png')} />
-            </View>
-
             <View style={style_01.divMain}>
-                <Text style={style_01.h1}>Demostración 01</Text>
-                <Text style={style_01.p1}>Desde el 2019, se está realizando el proceso de rediseño de la carrera, el cual implica la actualización de los contenidos en las asignaturas, así como las formas de mediación mas actuales, todo esto tomando en cuenta tambien diversas recomendaciones derivadas del proceso de autoevalación y acreditación de la carrera.</Text>
+                <Text style={style_01.h1}>Lista de Zodiacos</Text>
+                <Text style={style_01.p1}>En la astrología occidental se conoce como signos del zodiaco o astrológicos a los doce sectores de treinta grados exactos en los que los babilonios dividieron el camino que recorre el Sol alrededor de la Tierra y que hasta entonces se había segmentado en ocho partes.</Text>
 
                 <ScrollView style={{marginTop:8}}>
-                    {data.map(item => <View style={style_01.tarjeta} key={item.codigo} >
-                        <Text>Código: {item.codigo}</Text>
-                        <Text>Nombre: {item.nombre}</Text>
-                        <Text>Nivel: {item.nivel}</Text>
-                        <Text>Créditos: {item.creditos}</Text>
-                    </View>)}
+                    {data.map(item => <View style={style_01.tarjeta} key={item.Signo} >
+                        <Image source={require('../imgs/Signs/Air.jpeg')}/>
+                        <Text>Signo: {item.Signo}</Text>
+                        <Text>Dias que Abarca: {item.Dias}</Text>
+                        <Text>Elemento: {item.Elemento}</Text>
+                        <Text>Astro: {item.Astro}</Text>
+                        <Text> Piedra Preciosa: {item.Piedra_Preciosa}</Text>
+                    </View>)
+                    }
                 </ScrollView>
             </View>
 
